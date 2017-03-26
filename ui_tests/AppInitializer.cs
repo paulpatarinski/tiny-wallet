6e7664e196cfe8b271a829355ddc4a4347ac28b0
Paul Patarinski <paulpatarinski@gmail.com>
2017-03-26 13:01:33 -0500

    Adding ui tests

---
+++ ui_tests/AppInitializer.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using Xamarin.UITest;
+using Newtonsoft.Json;
+
+namespace ui_tests
+{
+    public class AppInitializer
+    {
+        private static Config _appConfig;
+        public static Config AppConfig
+        {
+            get
+            {
+                if (_appConfig == null)
+                {
+                    _appConfig = ParseConfig();
+                }
+                return _appConfig;
+            }
+        }
+
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .StartApp();
+            }
+
+            var deviceIdentifier = AppConfig.phone ? AppConfig.iphone_identifier : AppConfig.ipad_identifier;
+
+            return ConfigureApp
+                .iOS
+                .AppBundle(AppConfig.app_bundle_path)
+                .DeviceIdentifier(deviceIdentifier)
+                .EnableLocalScreenshots()
+                .StartApp();
+        }
+
+        private static Config ParseConfig()
+        {
+            return JsonConvert.DeserializeObject<PackageJson>(File.ReadAllText(@"../../package.json")).config;
+        }
+
+        public class PackageJson
+        {
+            public Config config { get; set; }
+        }
+
+        public class Config
+        {
+            public string app_bundle_path { get; set; }
+            public string iphone_identifier { get; set; }
+            public string ipad_identifier { get; set; }
+            public bool phone { get; set; }
+        }
+    }
+}
---
+++ ui_tests/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace ui_tests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void Generate_App_Screenshots()
+        {
+            app.Repl();
+        }
+
+        private void TakeScreenshot(string screenshotTitle)
+        {
+            var screenshotFileInfo = app.Screenshot(screenshotTitle);
+            var device = AppInitializer.AppConfig.phone ? "phone" : "tablet";
+            var newScreenshotName = screenshotFileInfo.Name.Replace("screenshot", device);
+            var newFilePath = $"{screenshotFileInfo.Directory}/{newScreenshotName}";
+            screenshotFileInfo.MoveTo(newFilePath);
+        }
+    }
+}
