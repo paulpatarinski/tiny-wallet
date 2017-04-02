05dff78a7e14e8e9680ff3f64664d1ada347cfd5
Paul Patarinski <paulpatarinski@gmail.com>
2017-04-02 18:44:25 -0500

    Adding an id to the card

--- src/models/card.ts
+++ src/models/card.ts
@@ -1,7 +1,9 @@
 import { Barcode } from "./barcode";
+import { UUID } from 'angular2-uuid';
 
 export class Card {
     constructor(barcode: Barcode, name: string, background: string, logoFileName: string, activated: Boolean) {
+        this.id = UUID.UUID();
         this.barcode = barcode;
         this.name = name;
         this.background = background;
@@ -10,6 +12,7 @@ export class Card {
         this.activated = activated;
     }
 
+    id: string;
     barcode: Barcode;
     name: string;
     background: string;
