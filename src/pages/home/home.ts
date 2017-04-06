560b73e2dcb8bb84899fec4652c7d15fdbf9c637
Paul Patarinski <paulpatarinski@gmail.com>
2017-04-06 01:54:21 -0500

    Remove unused ref

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ModalController, NavController, ScrollEvent } from 'ionic-angular';
+import { ModalController, NavController } from 'ionic-angular';
 import { CardTypeModal } from "../card-type-modal/card-type";
 import { Card } from "../../models/card";
 import { CardService } from "../../services/card.service";
